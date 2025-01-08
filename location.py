0cd4bbbfbc3c78bb32fa6fd35688d2e32f5e45f6
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2025-01-08 15:41:06 +0000

    formating fixes

--- location.py
+++ location.py
@@ -15,13 +15,18 @@ class Location:
         you_see: str = f"{self.name} \n {self.description}"
         lore_skill_check = True
         if lore_skill_check :
-            you_see += f"\n Ancient Lore : {self.lore}"
+            you_see += f"\n \n Ancient Lore : {self.lore}"
         return you_see
 
 # ------------ object creation ------------
 new_fort_cluckhaven = Location(name="New Fort Cluckhaven",
-                               description="An impressive bastion of saftey for all of the Chickens, Geese and Ducks of Apple Tree Yard. It has solid walls, anti-weasel infiltration screens, sunbeam pass-through projectors, and has two seperate sets of feed and water spots. Best of all, it has ample extra space for everyone to roost. Except for Cragglesnorts The Goose, of course, who refuses to climb up onto anything.",
-                               lore="Built by Mister Egg-Taker upon the ruins of the original Fort Cluckhaven, after Junior Egg-Taker destroyed the original early two Spring Times ago."
+                               description='''  An impressive bastion of saftey for all of the Chickens,
+ Ducks and Geese of Apple Tree Yard. It has solid walls, anti-weasel infiltration screens,
+ sunbeam pass-through projectors, and has two seperate sets of feed and water spots.
+    Best of all, it has ample extra space for everyone to roost. 
+    Except for Cragglesnorts The Goose, of course, who refuses to climb up onto anything.''',
+                               lore=''' Built by Mister Egg-Taker upon the ruins of the original Fort Cluckhaven,
+ after Junior Egg-Taker destroyed the original early two Spring Times ago.'''
                                )
 
 apple_tree_yard = Location(name="Apple Tree Yard",
